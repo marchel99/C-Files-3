b2256bbb9533f7a0b8dcb02fca902e82af093e8a
marchel99 <kubamarchelski@live.com>
2023-04-08 16:09:48 +0200

    Changes to be committed: 	modified:   main.c 	new file:   main.exe

--- main.c
+++ main.c
@@ -2,31 +2,48 @@
 
 int main (){
 
+int a=0;
 
-int a =5;
-char b='c';
-
-
-int *p=&a;
-char *pb=&b;
-
-printf("%p\n",*p);
+int *pa=&a;
 
 
-printf("Rozmiar int to: %d bajty\n",sizeof(int));
-printf("Rozmiar char to: %d bajty\n",sizeof(char));
-printf("Rozmiar float to: %d bajty\n",sizeof(float));
-printf("Rozmiar double to: %d bajty\n",sizeof(double));
-printf("Rozmiar unsigned int to: %d bajty\n",sizeof(unsigned int));
+struct book {
+char title[30];
+char author[30];
+char text [200];
+char desc [150];
+int date;
+};
 
-printf("%d\n",p+2);
 
 
-/* 
-printf("%p\n",*pb);
+struct book* b;
 
-printf("%c\n",*pb);
- */
+
+int c,d,e,f;
+
+int *pc=&c;
+int *pd=&d;
+int *pe=&e;
+int *pf=&f;
+
+
+printf("pc: %d\n",pc);
+printf("pd: %d\n",pd);
+printf("pc: %d\n",pe);
+printf("pd: %d\n",pf);
+
+printf("Size of this struct: %d\n",sizeof(struct book));
+printf("Size of the pointer to that struct: %d\n",sizeof(b));
+
+
+
+
+printf("Decimal: %d\n",pa);
+
+printf("Decimal: %d\n",&a);
+
+
 
     return 0;
 }
