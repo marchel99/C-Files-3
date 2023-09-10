08dd77efd7f3997b8e8075ab9cf55729996909c8
marchel99 <kubamarchelski@live.com>
2023-09-10 19:30:28 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -9,21 +9,11 @@ p=&a;
 int **q;
 q=&p;
 
-// int *p=&a;
-// RÓWNOZNACZNY Z
-// int *p=&a;
-
-
-
-
 
 printf("Adres pointeujr: %d Adresa %d\n",&p,&a);
 printf("Adres: %d zmiennej %d\n",p,*p);
 printf("200:%d 201:%d\n",*p,*p+1);
 printf("Adres*q:%d **q:%d",q,**q);
-
-
-
 
 
 
