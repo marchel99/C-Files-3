70ce36002efe7a848bbd41bc06538c7168952774
marchel99 <kubamarchelski@live.com>
2023-09-11 20:29:43 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -1,22 +1,10 @@
 #include <stdio.h>    
 
 int main (){
-int a= 200;
-
-int *p;
-p=&a;
-
-int **q;
-q=&p;
 
 
 
-printf("Adres pointeujr: %d Adresa %d\n",&p,&a);
-
-
-printf("Adres: %d zmiennej %d\n",p,*p);
-printf("200:%d 201:%d\n",*p,*p+1);
-printf("Adres*q:%d **q:%d",q,**q);
+    printf("ok");
 
 
 
