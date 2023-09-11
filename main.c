8b3e11ede5a145ca27171f94d904cd4999e7855e
marchel99 <kubamarchelski@live.com>
2023-09-11 18:32:03 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -10,7 +10,10 @@ int **q;
 q=&p;
 
 
+
 printf("Adres pointeujr: %d Adresa %d\n",&p,&a);
+
+
 printf("Adres: %d zmiennej %d\n",p,*p);
 printf("200:%d 201:%d\n",*p,*p+1);
 printf("Adres*q:%d **q:%d",q,**q);
