04eeea8a725e49d7c951a03d25cfebaeddfe037b
marchel99 <kubamarchelski@live.com>
2023-09-11 20:39:41 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
-#include <stdio.h>    
+#include <stdio.h>
 
-int main (){
+typedef int km;
 
+int main()
+{
 
-int a=1;
+    int a = 1;
 
+    km b = 2;
+    km c = 3;
 
-
-int table[]={1,2,3};
-
+    int table[] = {1, 2, 3};
 
     printf("ok\n");
 
+    for (int i = 0; i <= 2; ++i)
+    {
 
-for(  int i=0;i<=2 ;++i     )
-{
-
-    printf("Cyfra w tabeli: %d\n",table[i]);
-}
-
+        printf("Cyfra w tabeli: %d\n", table[i]);
+    }
+    printf("b+c= %d", (b + c));
     return 0;
 }
-
