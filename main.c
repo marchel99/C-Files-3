c10827444d6eac3fafd3fda89f8d752ee6bb6cd1
marchel99 <kubamarchelski@live.com>
2023-09-12 22:00:50 +0200

    modified:   main.c

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+#include <stdlib.h>
+
 typedef int km;
 
 int main()
 {
+
+    int *array;
+    array = malloc(1000 * sizeof(int));
+    for (int i = 0; i < 1000; ++i)
+
+        printf("%d", array[i]);
+
+    free(array);
+
 
     int a = 1;
 
@@ -12,13 +23,16 @@ int main()
 
     int table[] = {1, 2, 3};
 
-    printf("ok\n");
+  /*   printf("ok\n");
 
     for (int i = 0; i <= 2; ++i)
     {
-
         printf("Cyfra w tabeli: %d\n", table[i]);
     }
     printf("b+c= %d", (b + c));
+
+ */
+
+
     return 0;
 }
