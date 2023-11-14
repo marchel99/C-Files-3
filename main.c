43dc98f87b72187afb68686f6ce13b79a142a0e1
marchel99 <kubamarchelski@live.com>
2023-11-14 22:37:21 +0100

    ok

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-    enum Day{Sun=1,Mon=2,Tue=3,Wed=4,Thu=5,Fri=6,Sat=7};
-
-
 int main()
-
 
 
 {
  
- 
-    enum Day today=Mon;
-    printf("%d",today);
+int a=0;
+
+int *p;
+p=&a;
+
+
+printf("ok %d\n",p);
+printf("ok &d\n",a);
+
+
+
+
 
 
 
